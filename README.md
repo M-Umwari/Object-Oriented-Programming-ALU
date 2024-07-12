e28533ac6a7f54dd78f71ff321302245b81b92de
M-Umwari <m.umwari@alustudent.com>
2024-07-12 18:43:36 +0200

    complete tasks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# oops-javascript
+# University Residence Management System
+
+Objective:
+
+This assignment will challenge you to apply Object-Oriented Programming (OOP) concepts in JavaScript to model & design classes for a system for managing university residences. Your design should be based (but not limited to) on the following [requirements](<./Requirements%20Gathering_%20University%20Residence%20Management%20System%20(1).pdf>)
---
+++ residence.js
@@ -0,0 +1,73 @@
+// University class
+class University {
+  constructor(name) {
+    this.name = name;
+    this.residences = [];
+  }
+
+  addResidence(residence) {
+    this.residences.push(residence);
+  }
+
+  getResidence(name) {
+    return this.residences.find((residence) => residence.name === name);
+  }
+}
+
+// Residence class
+class Residence {
+  constructor(name) {
+    this.name = name;
+    this.students = [];
+    this.maintenanceRequests = [];
+  }
+
+  addStudent(student) {
+    this.students.push(student);
+  }
+
+  removeStudent(studentId) {
+    this.students = this.students.filter(
+      (student) => student.studentId !== studentId
+    );
+  }
+
+  submitMaintenanceRequest(request) {
+    this.maintenanceRequests.push(request);
+  }
+}
+
+// Student class
+class Student {
+  constructor(name, studentId) {
+    this.name = name;
+    this.studentId = studentId;
+  }
+}
+
+// MaintenanceRequest class
+class MaintenanceRequest {
+  constructor(description, student) {
+    this.description = description;
+    this.student = student;
+    this.status = 'Pending';
+  }
+
+  updateStatus(newStatus) {
+    this.status = newStatus;
+  }
+}
+
+// Example usage
+const university = new University('Example University');
+const residence = new Residence('Residence Hall 1');
+const student = new Student('John Doe', '12345');
+const request = new MaintenanceRequest('Leaky faucet', student);
+
+university.addResidence(residence);
+residence.addStudent(student);
+residence.submitMaintenanceRequest(request);
+
+console.log(university);
+console.log(residence);
+console.log(request);
